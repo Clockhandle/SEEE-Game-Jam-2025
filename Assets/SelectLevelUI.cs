3f80978cd4941204139070f64959644cce370a63
Clockhandle <155466922+Clockhandle@users.noreply.github.com>
2025-09-19 20:09:38 +0700

    add level_manager new

--- Assets/SelectLevelUI.cs
+++ Assets/SelectLevelUI.cs
@@ -7,13 +7,15 @@ using UnityEngine.UI;
 public class SelectLevelUI : MonoBehaviour
 {
     public Button[] button;
+    private Image lockLevelImg;
+
     private void Awake()
     {
         int unlockedLevel = PlayerPrefs.GetInt("unlockedLevel", 1);
         for (int i = 0; i < button.Length; i++)
         {
             button[i].interactable = false;
-
+            button[i].GetComponentInChildren<LockedImgReference>().gameObject.SetActive(true);
         }
 
         int maxUnlock = Mathf.Min(unlockedLevel, button.Length);
@@ -21,13 +23,21 @@ public class SelectLevelUI : MonoBehaviour
         for (int i = 0; i < maxUnlock; i++)
         {
             button[i].interactable = true;
+            button[i].GetComponentInChildren<LockedImgReference>().gameObject.SetActive(false);
             Debug.Log($"UnlockLevel number {i}");
         }
     }
 
     public void OpenLevel(int levelID)
     {
+        StartCoroutine(DelayTransis(levelID));
+    }
+
+    private IEnumerator DelayTransis(int levelID)
+    {
+        WhiteFlash.instance.ActiveFlashScreen();
+        yield return new WaitForSeconds(1f);
         string levelName = "Level" + levelID;
-        SceneManager.LoadScene(levelName);
+        LoadSceneManage.LoadLevel(levelName);
     }
 }
